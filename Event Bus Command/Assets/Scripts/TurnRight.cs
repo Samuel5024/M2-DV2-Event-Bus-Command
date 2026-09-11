5ff5e1fcea0a1780a56442e0a662d41e9669238c
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2026-09-11 16:08:15 -0700

    Add TurnRight.cs

---
+++ Event Bus Command/Assets/Scripts/TurnRight.cs
@@ -0,0 +1,17 @@
+using Chapter.EventBus;
+using UnityEngine;
+
+public class TurnRight : Command
+{
+    private BikeController _controller;
+
+    public TurnRight(BikeController controller)
+    {
+        _controller = controller;
+    }
+
+    public override void Execute()
+    {
+        _controller.Turn(BikeController.Direction.Right);
+    }
+}
