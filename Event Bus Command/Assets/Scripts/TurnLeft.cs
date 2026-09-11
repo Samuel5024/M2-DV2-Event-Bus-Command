ec39c919d3743e4ad12b0687a2a78ba837fc314e
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2026-09-11 16:05:48 -0700

    Add TurnLeft.cs

---
+++ Event Bus Command/Assets/Scripts/TurnLeft.cs
@@ -0,0 +1,16 @@
+using Chapter.EventBus;
+using UnityEngine;
+
+public class TurnLeft : Command
+{
+    private BikeController _controller;
+
+    public TurnLeft(BikeController controller)
+    {
+        _controller = controller;
+    }
+    public override void Execute()
+    {
+        _controller.Turn(BikeController.Direction.Left);
+    }
+}
