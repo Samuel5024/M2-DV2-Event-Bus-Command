ddc091da783c7cd1b7a6a9004ff865a75dfbb2d7
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2026-09-11 12:06:19 -0700

    Update Command.cs

--- Event Bus Command/Assets/Scripts/Command.cs
+++ Event Bus Command/Assets/Scripts/Command.cs
@@ -1,16 +1,4 @@
-using UnityEngine;
-
-public class Command : MonoBehaviour
+public abstract class Command
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public abstract void Execute();
 }
